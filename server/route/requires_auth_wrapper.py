06ec328ada5574182d300c11b53a208709efce2e
lly02 <lly.02@outlook.com>
2025-08-09 19:03:59 +0800

    feat: change session to id instead of email

--- server/route/requires_auth_wrapper.py
+++ server/route/requires_auth_wrapper.py
@@ -3,19 +3,20 @@ from flask import jsonify, session, g
 from server.model.tables import AccountType
 from server.model.service.common_service import CommonService
 
-def requires_auth(method):
-    @wraps(method)
-    def wrapper(account_type: AccountType, *args, **kwargs):
-        if (
-            "session" not in session
-            or session["session"]["account_type"] != account_type
-            or not CommonService(g.Session).verify(
-                session["session"]["email"], 
-                session["session"]["account_type"]
-            )
-        ):
-            return jsonify({"message": "Unauthorised"}), 401
-        
-        return method(*args, **kwargs)
-        
-    return wrapper
+def requires_auth(account_type):
+    def decorator(method):
+        @wraps(method)
+        def wrapper(*args, **kwargs):
+            if (
+                "session" not in session
+                or session["session"]["account_type"] != account_type
+                or not CommonService(g.Session).verify(
+                    session["session"]["id"], 
+                    session["session"]["account_type"]
+                )
+            ):
+                return jsonify({"message": "Unauthorized"}), 401
+
+            return method(*args, **kwargs)
+        return wrapper
+    return decorator
