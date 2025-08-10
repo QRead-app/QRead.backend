4872a1887eec0ca871ffab82a8dcff271c7ef765
lly02 <lly.02@outlook.com>
2025-08-11 00:05:52 +0800

    feat(api): Add sessionmaker autocommit configuration

--- server/model/db.py
+++ server/model/db.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 class DB:
-    def __init__(self, connection_string):
+    def __init__(self, connection_string, autocommit=True):
         self.engine = create_engine(connection_string)
-        self.session = sessionmaker(self.engine)
+        self.session = sessionmaker(self.engine, autocommit=autocommit)
 
     def get_sessionmaker(self) -> sessionmaker:
         return self.session
