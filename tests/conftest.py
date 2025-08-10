7b1d94099553bd7234d379bbadd48553a54f9fc8
lly02 <lly.02@outlook.com>
2025-08-10 23:15:05 +0800

    fix(test): Fix test fixtures app context

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,44 +1,62 @@
 import pytest
 
-from flask import g
+from flask import g 
+from server.model.db import DB
 from server.model.tables import AccountType
 from tests.test_users import test_borrower, test_librarian, test_admin
 from server import create_app
 from server.model.repository.user_account_repository import UserAccountRepository
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def app():
     app = create_app()
 
-    with app.app_context():
-        user_repo = UserAccountRepository(g.Session)
+    @app.before_request
+    def load_session():
+        g.Session = Session
 
-        # Insert test users
-        g.borrower = user_repo.insert_user(
-            name = test_borrower.name,
-            email = test_borrower.email,
-            password = test_borrower.password,
-            type = test_borrower.account_type
-        )
-        g.librarian = user_repo.insert_user(
-            name = test_librarian.name,
-            email = test_librarian.email,
-            password = test_librarian.password,
-            type = test_librarian.account_type
-        )
-        g.admin = user_repo.insert_user(
-            name = test_admin.name,
-            email = test_admin.email,
-            password = test_admin.password,
-            type = test_admin.account_type
-        )
+    with app.app_context():
+        db = DB(app.config["CONNECTION_STRING"])
+        Session = db.get_sessionmaker()
+
+        with Session.begin() as session:
+            user_repo = UserAccountRepository(session)
+
+            # Insert test users
+            borrower = user_repo.insert_user(
+                name = test_borrower.name,
+                email = test_borrower.email,
+                password = test_borrower.password,
+                type = test_borrower.account_type
+            )
+            librarian = user_repo.insert_user(
+                name = test_librarian.name,
+                email = test_librarian.email,
+                password = test_librarian.password,
+                type = test_librarian.account_type
+            )
+            admin = user_repo.insert_user(
+                name = test_admin.name,
+                email = test_admin.email,
+                password = test_admin.password,
+                type = test_admin.account_type
+            )
+
+        app.test_users = {
+            "borrower": borrower,
+            "librarian": librarian,
+            "admin": admin,
+        }
 
         yield app
 
-        # Clean up test users
-        user_repo.delete_user(g.borrower)
-        user_repo.delete_user(g.librarian)
-        user_repo.delete_user(g.admin)
+        with Session.begin() as session:
+            user_repo = UserAccountRepository(session)
+            
+            # Clean up test users
+            user_repo.delete_user(app.test_users["borrower"])
+            user_repo.delete_user(app.test_users["librarian"])
+            user_repo.delete_user(app.test_users["admin"])
 
 
 @pytest.fixture
