cdc265f17196d1a7863a0bc4c8abc00dc142a845
lly02 <lly.02@outlook.com>
2025-08-11 00:09:28 +0800

    fix(api): Removed sessionmaker configuration

--- server/model/db.py
+++ server/model/db.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 class DB:
-    def __init__(self, connection_string, autocommit=True):
+    def __init__(self, connection_string):
         self.engine = create_engine(connection_string)
-        self.session = sessionmaker(self.engine, autocommit=autocommit)
+        self.session = sessionmaker(self.engine)
 
     def get_sessionmaker(self) -> sessionmaker:
         return self.session
