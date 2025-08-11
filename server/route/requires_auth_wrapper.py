a18aba5fc31d9d7c2d0edb85a38b5bf3d0d330a3
lly02 <lly.02@outlook.com>
2025-08-11 18:03:54 +0800

    Fix: Fix requires auth wrapper account type check logic

--- server/route/requires_auth_wrapper.py
+++ server/route/requires_auth_wrapper.py
@@ -3,13 +3,13 @@ from flask import jsonify, session, g
 from server.model.tables import AccountType
 from server.model.service.common_service import CommonService
 
-def requires_auth(account_type):
+def requires_auth(account_type: AccountType):
     def decorator(method):
         @wraps(method)
         def wrapper(*args, **kwargs):
             if (
                 "session" not in session
-                or session["session"]["account_type"] != account_type
+                or session["session"]["account_type"] != account_type.name
                 or not CommonService(g.Session).verify(
                     session["session"]["id"], 
                     session["session"]["account_type"]
