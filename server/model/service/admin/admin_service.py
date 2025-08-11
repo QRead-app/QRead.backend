75dfa14aae47076087ca5f2ce732214610f0547a
lly02 <lly.02@outlook.com>
2025-08-11 23:18:55 +0800

    feat(api): Admin - Retrieve, Suspend, Reinstate user

--- server/model/service/admin/admin_service.py
+++ server/model/service/admin/admin_service.py
@@ -1,6 +1,6 @@
 from server.model.service.base_service import BaseService
 from server.model.service.transactional_wrapper import transactional
-from server.model.tables import AccountType
+from server.model.tables import AccountState, AccountType, User
 from server.model.repository.user_account_repository import UserAccountRepository
 from sqlalchemy.exc import NoResultFound
 
@@ -9,12 +9,30 @@ class AdminService(BaseService):
         super().__init__(Session)
 
     @transactional
-    def authenticate(self, email: str, password: str) -> bool:
-            user_repo = UserAccountRepository(self.session)
+    def get_users(
+        self, 
+        id: int = None, 
+        name: str = None,
+        email: str = None,
+        account_type: AccountType = None,
+        account_state: AccountState = None
+    ) -> list[User]:
+        return UserAccountRepository(self.session).get_user(
+            id=id,
+            name=name,
+            email=email,
+            account_type=account_type,
+            account_state=account_state
+        )
 
-            try:
-                user_repo.get_user_by_email_password(email, password, AccountType.ADMIN)
-            except NoResultFound: 
-                return False
-            
-            return True
+    @transactional
+    def suspend_user(self, user: User) -> User:
+        user.account_state = AccountState.SUSPENDED
+
+        return user
+    
+    @transactional
+    def reinstate_user(self, user: User) -> User:
+        user.account_state = AccountState.ACTIVE
+
+        return user
--- server/route/admin/admin.py
+++ server/route/admin/admin.py
@@ -1,4 +1,61 @@
-from flask import Blueprint
+from flask import Blueprint, g, jsonify, request
+
+from server.exceptions import ConversionError, DatabaseError
+from server.model.service.admin.admin_service import AdminService
+from server.model.tables import AccountType, User
+from server.route.requires_auth_wrapper import requires_auth
 
 admin = Blueprint('admin', __name__, url_prefix="/admin")
 
+@admin.route("/new-librarian", methods=["POST"])
+@requires_auth(AccountType.ADMIN)
+def new_librarian():
+    pass
+
+@admin.route("/get-users", methods=["GET"])
+@requires_auth(AccountType.ADMIN)
+def get_users():
+    try:
+        users = AdminService(g.Session).get_users()
+    except DatabaseError as e:
+        return jsonify({"error": str(e)}), 500
+
+    return users
+
+@admin.route("/suspend-user", methods=["POST"])
+@requires_auth(AccountType.ADMIN)
+def suspend_user():
+    data = request.json
+    user_id = data.get("id")
+
+    try:
+        user_id = User.str_to_int(user_id)
+    except ConversionError as e:
+        return jsonify({"error": str(e)}), 400
+
+    try:
+        user = AdminService(g.Session).get_users(id=user_id)
+        AdminService(g.Session).suspend_user(user)
+    except DatabaseError as e:
+        return jsonify({"error": str(e)}), 500
+
+    return user
+
+@admin.route("/reinstate-user", methods=["POST"])
+@requires_auth(AccountType.ADMIN)
+def reinstate_user():
+    data = request.json
+    user_id = data.get("id")
+
+    try:
+        user_id = User.str_to_int(user_id)
+    except ConversionError as e:
+        return jsonify({"error": str(e)}), 400
+
+    try:
+        user = AdminService(g.Session).get_users(id=user_id)
+        AdminService(g.Session).reinstate_user(user)
+    except DatabaseError as e:
+        return jsonify({"error": str(e)}), 500
+
+    return user
