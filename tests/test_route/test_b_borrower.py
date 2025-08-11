c5bdde02c15527add1d161555ccb3ca2a3a5bc84
lly02 <lly.02@outlook.com>
2025-08-11 18:26:42 +0800

    feat(test): Borrower - Borrow

--- tests/test_route/test_b_borrower.py
+++ tests/test_route/test_b_borrower.py
@@ -1,6 +1,7 @@
 import pytest
 
-from tests.test_data import test_admin, test_borrower, test_librarian
+from flask import current_app, g
+from tests.test_data import *
 
 @pytest.mark.parametrize(('name', 'email', 'password', 'message', "code"), (
     (None, None, None, "Missing fields", 400),
@@ -40,17 +41,20 @@ def test_borrow_bad_unauthorized(client):
     response = client.post(
         "/borrower/borrow",
         json = {
-            "books": [  ]
+            "books": ["123"]
         }
     )
 
+    assert response.json.get("error") == "Unauthorized"
+    assert response.status_code == 401
+
 @pytest.mark.parametrize(('books', 'message', "code"), (
-    (None, "Missing fields", 400),
-    ({"books": [99999]}, "99999 has already been borrowed or does not exist", 400),
-    ({"books": [88888, 99999]}, "88888 has already been borrowed or does not exist", 400)
+    ({}, "Missing field", 400),
+    ({"books": ["1"]}, "Book 1 does not exist", 400),
+    ({"books": ["2", "1"]}, "Book 2 does not exist", 400)
 ))
-def test_borrow_bad(client, books, message, code):
-    response = client.post(
+def test_borrow_bad(borrower_client, books, message, code):
+    response = borrower_client.post(
         "/borrower/borrow",
         json = books
     )
@@ -58,10 +62,25 @@ def test_borrow_bad(client, books, message, code):
     assert response.json.get("error") == message
     assert response.status_code == code
 
-def test_borrow_good(client):
-    response = client.post(
-        "borrower/borrow",
+def test_borrow_good(borrower_client):
+    response = borrower_client.post(
+        "/borrower/borrow",
         json = {
-            "books": []
+            "books": [current_app.config["test_book"]["id"]]
         }
     )
+
+    assert response.json.get("message") == "Book(s) borrowed successfully"
+    assert response.status_code == 200
+
+def test_borrow_bad_borrowed(borrower_client):
+    response = borrower_client.post(
+        "/borrower/borrow",
+        json = {
+            "books": [current_app.config["test_book"]["id"]]
+        }
+    )
+
+    assert response.json.get("error") == f"Book {current_app.config['test_book']['id']} has already been borrowed"
+    assert response.status_code == 400
+
