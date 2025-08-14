ae225e9aaa000acadc869668c991b828d5771fc7
lly02 <lly.02@outlook.com>
2025-08-14 14:53:27 +0800

    refactor: Admin type checking and error handling

--- server/route/admin/admin.py
+++ server/route/admin/admin.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, g, jsonify, request
 
-from server.exceptions import ConversionError, DatabaseError
+from server.exceptions import ConversionError, DatabaseError, RecordNotFoundError
 from server.model.service.admin.admin_service import AdminService
 from server.model.tables import AccountType, User
 from server.route.requires_auth_wrapper import requires_auth
@@ -15,12 +15,16 @@ def new_librarian():
 @admin.route("/get-users", methods=["GET"])
 @requires_auth(AccountType.ADMIN)
 def get_users():
-    try:
-        users = AdminService(g.Session).get_users()
-    except DatabaseError as e:
-        return jsonify({"error": str(e)}), 500
+    users = AdminService(g.Session).get_users()
 
-    return users
+    data = []
+    for user in users:
+        data.append(user.to_dict())
+
+    return jsonify({
+        "message": "Users retreived",
+        "data": data
+    }), 200
 
 @admin.route("/suspend-user", methods=["POST"])
 @requires_auth(AccountType.ADMIN)
@@ -28,18 +32,22 @@ def suspend_user():
     data = request.json
     user_id = data.get("id")
 
+    if user_id is None:
+        return jsonify({"error": f"Missing id field"})
+
     try:
         user_id = User.str_to_int(user_id)
     except ConversionError as e:
-        return jsonify({"error": str(e)}), 400
+        return jsonify({"error": f"Invalid id {user_id}"}), 400
 
     try:
         user = AdminService(g.Session).get_users(id=user_id)
-        AdminService(g.Session).suspend_user(user)
-    except DatabaseError as e:
-        return jsonify({"error": str(e)}), 500
+    except RecordNotFoundError:
+        return jsonify({"error": f"User id {user_id} not found"}), 404
 
-    return user
+    AdminService(g.Session).suspend_user(user)
+
+    return jsonify({"message": f"User suspended succesfully"}), 200
 
 @admin.route("/reinstate-user", methods=["POST"])
 @requires_auth(AccountType.ADMIN)
@@ -47,15 +55,19 @@ def reinstate_user():
     data = request.json
     user_id = data.get("id")
 
+    if user_id is None:
+        return jsonify({"error": f"Missing id field"})
+
     try:
         user_id = User.str_to_int(user_id)
     except ConversionError as e:
-        return jsonify({"error": str(e)}), 400
+        return jsonify({"error": f"Invalid id {user_id}"}), 400
 
     try:
         user = AdminService(g.Session).get_users(id=user_id)
-        AdminService(g.Session).reinstate_user(user)
-    except DatabaseError as e:
-        return jsonify({"error": str(e)}), 500
+    except RecordNotFoundError:
+        return jsonify({"error": f"User id {user_id} not found"}), 404
+    
+    AdminService(g.Session).reinstate_user(user)
 
-    return user
+    return jsonify({"message": f"User reinstated succesfully"}), 200
