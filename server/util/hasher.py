40a1f72df28929816708aa5f6610bc9c6dfc6cf0
lly02 <lly.02@outlook.com>
2025-08-15 23:32:50 +0800

    feat: Password hasher using argon2

---
+++ server/util/hasher.py
@@ -0,0 +1,30 @@
+from argon2 import PasswordHasher
+from argon2.exceptions import VerifyMismatchError, HashingError
+
+from server.exceptions import DatabaseError, IncorrectCredentialsError
+
+class Hasher:
+    def __init__(self):
+        # Preset according to recommendation from 
+        # https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html
+        self.hasher = PasswordHasher(
+            time_cost = 2,
+            memory_cost = 19456,
+            parallelism = 1,
+        )
+
+    def hash(self, password: str) -> str:
+        try:
+            hash = self.hasher.hash(password)
+        except HashingError as e:
+            raise DatabaseError()
+
+        return hash
+    
+    def verify(self, hash: str, password: str) -> bool:
+        try:    
+            verification = self.hasher.verify(hash, password)
+        except VerifyMismatchError as e:
+            raise IncorrectCredentialsError()
+        
+        return verification
