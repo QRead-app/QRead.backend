796b7d2551dce169b3a22c03210642f938e29467
lly02 <lly.02@outlook.com>
2025-08-17 22:06:07 +0800

    feat(db): App settings repo

---
+++ server/model/repository/app_settings_repository.py
@@ -0,0 +1,32 @@
+from ..tables import AppSettings, Book, BookCondition
+from .base_repository import BaseRepository
+from sqlalchemy import or_, select, text
+from sqlalchemy.orm import Session
+
+class BookRepository(BaseRepository):
+    def __init__(self, session: Session):
+        super().__init__(session)
+    
+    def get_setting(
+        self, 
+        key: str
+    ) -> list[AppSettings]:
+        return self.session.execute(
+            select(AppSettings).where(AppSettings.key == key)
+        ).scalars().all()
+
+    def insert_setting(
+        self, 
+        key: str,
+        value: str
+    ) -> AppSettings:
+        setting = AppSettings(key = key, value = value)
+        self.session.add(setting)
+        
+        return setting
+
+    def delete_setting(self, setting: AppSettings) -> None:
+        self.session.delete(setting)  
+
+    def truncate_table(self) -> None:
+        self.session.execute(text("TRUNCATE TABLE app_settings CASCADE"))
