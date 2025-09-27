e4893e431bdf8409d53253d1b18491d41cb54e5c
lly02 <lly.02@outlook.com>
2025-09-27 11:34:24 +0800

    feat: allow access as long as autenticated

--- server/route/requires_auth_wrapper.py
+++ server/route/requires_auth_wrapper.py
@@ -3,13 +3,24 @@ from flask import jsonify, session, g
 from server.model.tables import AccountType
 from server.model.service.common_service import CommonService
 
-def requires_auth(account_type: AccountType):
+def requires_auth(account_type: AccountType | None):
+    def auth_type_check(type: AccountType | None, check: AccountType) -> bool:
+        if type is not None:
+            return type == check
+        
+        return type in AccountType
+
     def decorator(method):
         @wraps(method)
         def wrapper(*args, **kwargs):
+            auth_type = None
+            
+            if account_type is not None:
+                auth_type = account_type.name
+                
             if (
                 "session" not in session
-                or session["session"]["account_type"] != account_type.name
+                or auth_type_check(session["session"]["account_type"], auth_type)
                 or not CommonService(g.Session).verify(
                     session["session"]["id"], 
                     session["session"]["account_type"]
